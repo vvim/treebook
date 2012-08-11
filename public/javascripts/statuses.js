2d0ce36ed08a71bcadb9aebff9ab321d3b9f966a
Wim Deprez <wim@delta.(none)>
2012-08-11 22:28:43 +0200

    Added bootstrap, and applied layout to the page

---
+++ public/javascripts/statuses.js
@@ -0,0 +1,10 @@
+var jQuery = window.jQuery = window.$ = function(selector, context)
+    {
+    };
+    
+$(function() {
+          return $('.status').hover(function(event) {
+            return $(this).toggleClass("hover");
+          });
+        });
+
---
+++ public/javascripts/statuses.js.coffee
@@ -0,0 +1,7 @@
+//= require jquery
+//= require jquery_ujs
+//= require_tree .
+
+$ ->
+    $('.status').hover (event) ->
+        console.log("Hover triggered")
